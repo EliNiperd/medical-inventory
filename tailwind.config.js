10ec4c6d4ce9ef96708325743d2577c98738852f
EliNiperd <122644830+EliNiperd@users.noreply.github.com>
2024-03-01 23:54:09 -0600

    (add) - first commit from local: ERS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +6,49 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))",
+      },
+      colors: {
+        alert: {
+          100: "#FCF3F2",
+          200: "#FADCD9",
+          300: "#FABBB4",
+          400: "#FC9086",
+          500: "#FA5343",
+          600: "#D91F11",
+          700: "#A1160A",
+          800: "#75160C",
+          900: "#4F150F",
+          1000: "#24120C",
+        },
+        primary: {
+          25: "#F1F4FE",
+          50: "#E6ECFE",
+          100: "#D4DBFC",
+          200: "#A9B8FA",
+          300: "#7C90F1",
+          400: "#5A6FE4",
+          500: "#2940D3",
+          600: "#1D30B5",
+          700: "#142297",
+          800: "#0D177A",
+          900: "#070F65",
+        },
+        blue: {
+          400: "#2589FE",
+          500: "#0070F3",
+          600: "#2F6FEB",
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        "100%": {
+          transform: "translateX(100%)",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
