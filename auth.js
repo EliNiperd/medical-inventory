01ee0abed012c5eed23e7407bd02707e63af31c6
EliNiperd-Huawei <eli.rodriguez@niperd.mx>
2025-12-12 21:39:39 -0600

    fix auth trustedHosts

--- auth.js
+++ auth.js
@@ -1,10 +1,7 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import bcrypt from 'bcrypt';
-//import { sql } from "@vercel/postgres";
 import sql from '@/app/lib/postgresql';
-//import { prisma } from "@/app/lib/prisma";
-//import { z } from "zod";
 import { authConfig } from './auth.config';
 
 async function getUser(email) {
@@ -13,51 +10,49 @@ async function getUser(email) {
 
     return user[0];
   } catch (error) {
-    //pool.end();
     // console.error('Failed to fetch user:', error); // 🔍Usar solo para Debug
     throw new Error('Failed to fetch user.', error);
   }
 }
-export const { auth, signIn, signOut } = NextAuth(
-  Object.assign(Object.assign({}, authConfig), {
-    providers: [
-      Credentials({
-        async authorize(credentials) {
-          //const { email, password } = credentials;
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
 
-          const user = await getUser(credentials.email);
-          if (!user) return null;
+  // ✅ Asegúrate de que trustedHosts esté AQUÍ
+  trustedHosts: ['medical-inventory.eliconacento.com', 'www.medical-inventory.eliconacento.com'],
 
-          // Comparar la contraseña proporcionada con el hash almacenado
-          const passwordsMatch = bcrypt.compare(credentials.password, user.password);
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const user = await getUser(credentials.email);
+        if (!user) return null;
 
-          //console.log(passwordsMatch, user);
-          if (passwordsMatch) return user;
-          // }
+        const passwordsMatch = bcrypt.compare(credentials.password, user.password);
+        if (passwordsMatch) return user;
 
-          //console.log("Invalid credentials");
-          return null;
-        },
-      }),
-    ],
-    session: {
-      jwt: true,
-    },
-    callbacks: {
-      jwt({ token, user }) {
-        if (user) {
-          token.id = user.id;
-          token.email = user.email;
-        }
-        return token;
+        return null;
       },
-      session({ session, token }) {
-        session.user = token;
-        return session;
-      },
+    }),
+  ],
+
+  session: {
+    jwt: true,
+  },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
     },
-    pages: {
-      signIn: '/login',
+    session({ session, token }) {
+      session.user = token;
+      return session;
     },
-  })
-);
+  },
+
+  pages: {
+    signIn: '/login',
+  },
+});
